5e99d340c7ae3a225d6f1068c88d1cff36cbb4be
BytDance <arccmina@gmail.com>
2024-11-27 10:37:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { set1, set2 } from './drums.js';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
-  return (
+
+    const [keyPressed, setKeyPressed] = useState('');
+    const [currentSet, setKeySet] = useState(set1);
+    const [keySetSwitch, setKeySwitch] = useState(true);
+
+    useEffect(() => {
+        const keypressed = (event) => {
+            let a = document.getElementById(event.key.toUpperCase());
+            if (a) { playAudio(a); }
+        };
+
+        const keyUp = (event) => {
+            let b = document.getElementById(event.key.toUpperCase());
+            if (b && b.classList.contains('drumpadOn')) { keyUpStyle(b); }
+        };
+        document.addEventListener('keyup', keyUp);
+        document.addEventListener('keydown', keypressed);
+        return () => {
+            document.removeEventListener('keydown', keypressed);
+            document.removeEventListener('keyup', keyUp);
+        };
+    }, []);
+    function switchSets(event) {
+        if (keySetSwitch) { event.currentTarget.className = "drumpadOn"; setKeySwitch(false); setKeySet(set2); }
+        else { event.currentTarget.className = "drumpadOff"; setKeySwitch(true); setKeySet(set1); }
+    }
+    function mouseUp (event) { keyUpStyle(event.currentTarget); }
+    function mouseDown(event) { playAudio(event.target); }
+    function keyUpStyle(b) { b.className = "drum-pad drumpadOff";}
+    function playAudio(t) {
+        t.className = "drum-pad drumpadOn";
+        setKeyPressed(t.title);
+        t.querySelector('audio').play();
+    }
+
+    
+
+    return (
+       
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="App-container">
+              <div id="drum-machine">
+                    <div id="display">
+                        <div id="drumpads">
+                            {
+                                
+                                currentSet.map(i => (
+                                <div
+                                    onMouseDown={mouseDown}
+                                    className="drum-pad drumpadOff"
+                                    title={i.id}
+                                    id={i.keyTrigger}
+                                    onMouseUp={mouseUp}>
+                                    {i.keyTrigger}
+                                    <audio id={i.keyTrigger}
+                                        className="clip"
+                                        src={i.url}></audio>
+                                </div>))}
+
+                        </div>
+                        <button id="setSwitcherId" className="drumpadOff " onClick={switchSets}>Set 2</button>
+                        <h4 id="title">{keyPressed}</h4>
+                    </div>
+                </div>
+               
+            </div>
+            
     </div>
   );
 }
